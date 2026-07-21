df0e9876361423aecf277e73c29ca2ba3a4f64b6
Adam Elmadani <adamelmadani0129@gmail.com>
2026-07-21 17:09:42 +0100

    restyle everything

--- src/main/java/com/smartbook/security/WebSecurityConfig.java
+++ src/main/java/com/smartbook/security/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
             .authorizeRequests()
-                .antMatchers("/", "/home", "/signup", "/signin", "/css/**", "/js/**", "/images/**").permitAll()
+                .antMatchers("/", "/home", "/signup", "/signin", "/search", "/discover", "/book/**", "/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
             .and()
             .formLogin()
